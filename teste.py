c1ad355d51c4f3410dc4a64c7e954f5dadd76463
Elton-morais12 <eltonsouzademorais@gmail.com>
2025-11-20 15:22:14 -0300

    Iniciar o repositorio de atividades

---
+++ teste.py
@@ -0,0 +1,9 @@
+# # ler numero digitado pelo usuario
+# numero = int(input('Digite um numero: '))
+# # Calcular anterior e sucessor
+# anterior = numero - 1
+# sucessor = numero + 1
+# # Mostrar resultados
+# print('numero digitado', numero)
+# print('Anterior:', anterior)
+# print('Sucessor', sucessor)
